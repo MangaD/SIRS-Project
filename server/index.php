ef270ad2cbcfa575be0a49cfeb2a570f3c558a81
David Gonçalves <davidmanuel1993@hotmail.com>
2019-11-16 12:14:07 +0000

    working on server

--- server/index.php
+++ server/index.php
@@ -34,43 +34,57 @@
 
 		<!-- Template from: https://bootsnipp.com/snippets/vl4R7 -->
 		<div class="container">
-			<div class="d-flex justify-content-center h-100">
-				<div class="card">
-					<div class="card-header">
-						<h3>Sign In</h3>
-					</div>
-					<div class="card-body">
-						<form>
-							<div class="input-group form-group">
-								<div class="input-group-prepend">
-									<span class="input-group-text"><i class="fas fa-user"></i></span>
-								</div>
-								<input type="text" class="form-control" placeholder="username">
-								
-							</div>
-							<div class="input-group form-group">
-								<div class="input-group-prepend">
-									<span class="input-group-text"><i class="fas fa-key"></i></span>
-								</div>
-								<input type="password" class="form-control" placeholder="password">
-							</div>
-							<div class="row align-items-center remember">
-								<input type="checkbox">Remember Me
-							</div>
-							<div class="form-group">
-								<input type="submit" value="Login" class="btn float-right login_btn" onclick="sendMessage('login');">
-							</div>
-						</form>
-					</div>
-					<div class="card-footer">
-						<div class="d-flex justify-content-center links">
-							Don't have an account?<a href="#">Sign Up</a>
+
+			<h1>Smartphone as a security token</h1>
+
+			<div class="container">
+				<div class="d-flex justify-content-center h-100">
+
+					<div class="card">
+						<div class="card-header">
+							<h3>Sign In</h3>
 						</div>
-						<div class="d-flex justify-content-center">
-							<a href="#">Forgot your password?</a>
+						<div class="card-body">
+							<form>
+								<div class="input-group form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text">
+											<i class="fas fa-user"></i></span>
+									</div>
+									<input type="text" class="form-control"
+										placeholder="username">
+									
+								</div>
+								<div class="input-group form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text">
+											<i class="fas fa-key"></i></span>
+									</div>
+									<input type="password" class="form-control"
+										placeholder="password">
+								</div>
+								<div class="row align-items-center remember">
+									<input type="checkbox">Remember Me
+								</div>
+								<div class="form-group">
+									<input type="submit" value="Login"
+										class="btn float-right login_btn"
+										onclick="sendMessage('login');">
+								</div>
+							</form>
+						</div>
+						<div class="card-footer">
+							<div class="d-flex justify-content-center links">
+								Don't have an account?<a href="#">Sign Up</a>
+							</div>
+							<div class="d-flex justify-content-center">
+								<a href="#">Forgot your password?</a>
+							</div>
 						</div>
 					</div>
+
 				</div>
+
 			</div>
 		</div>
 
@@ -91,7 +105,7 @@
 		<script src="js/particles.min.js"></script>
 		<script>
 			particlesJS.load('particles-js', 'js/particles.json', function() {
-				console.log('callback - particles.js config loaded');
+				//console.log('callback - particles.js config loaded');
 			});
 		</script>
 	</body>
