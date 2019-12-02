128562c26e03c745da055b679041bbc5c5db7a12
David Gonçalves <davidmanuel1993@hotmail.com>
2019-12-02 20:49:40 +0000

    upload multiple files

--- server/files.php
+++ server/files.php
@@ -43,85 +43,97 @@ if (!isInstalled()) {
 	$errors['not_logged'] = 'You are not logged in!';
 } elseif ($_SERVER['REQUEST_METHOD'] != 'POST') {
 	$errors['post'] = 'Must send data over POST request method.';
+} elseif (!isset($_FILES['files'])) {
+	$errors['_FILES'] = "\$_FILES['files'] not set.";
 }
 
 if (empty($errors)) {
-	if (isset($_FILES['files'])) {
 
-		$all_files = count($_FILES['files']['tmp_name']);
+	$all_files = count($_FILES['files']['tmp_name']);
 
-		for ($i = 0; $i < $all_files; $i++) {
-			$file_name = $_FILES['files']['name'][$i];
-			// https://stackoverflow.com/questions/37008227/what-is-the-difference-between-name-and-tmp-name
-			$file_tmp = $_FILES['files']['tmp_name'][$i];
-			$file_type = $_FILES['files']['type'][$i];
-			$file_size = $_FILES['files']['size'][$i];
-			// https://stackoverflow.com/questions/173868/how-do-i-get-extract-a-file-extension-in-php
-			$file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+	for ($i = 0; $i < $all_files; $i++) {
+		
+		$file_name = $_FILES['files']['name'][$i];
+		// https://stackoverflow.com/questions/37008227/what-is-the-difference-between-name-and-tmp-name
+		$file_tmp = $_FILES['files']['tmp_name'][$i];
+		$file_type = $_FILES['files']['type'][$i];
+		$file_size = $_FILES['files']['size'][$i];
+		// https://stackoverflow.com/questions/173868/how-do-i-get-extract-a-file-extension-in-php
+		$file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-			$file = $folderPath . $file_name;
+		$file = $folderPath . $file_name;
 
-			if (!in_array($file_ext, $validExtensions)) {
-				$errors['extension'] = 'Extension not allowed. File name: ' . $file_name .
-					'. File extension: ' . $file_ext;
-			}
+		if (!in_array($file_ext, $validExtensions)) {
+			$errors['extension'] = 'Extension not allowed. File name: ' . $file_name .
+				'. File extension: ' . $file_ext;
+		}
 
-			if (!in_array($file_type, $validTypes)) {
-				$errors['size'] = 'File type not allowed. File name: ' . $file_name .
-				'. File type: ' . $file_type;
-			}
+		if (!in_array($file_type, $validTypes)) {
+			$errors['size'] = 'File type not allowed. File name: ' . $file_name .
+			'. File type: ' . $file_type;
+		}
 
-			if ($file_size > $maxFileSize) {
-				$errors['size'] = 'File size exceeds limit. File name: ' . $file_name .
-					'. File size: ' . $file_size .
-					'. Max. size: ' . $maxFileSize/1024/1024 . "MiB";
-			}
+		if ($file_size > $maxFileSize) {
+			$errors['size'] = 'File size exceeds limit. File name: ' . $file_name .
+				'. File size: ' . $file_size .
+				'. Max. size: ' . $maxFileSize/1024/1024 . "MiB";
+		}
+	}
 
-			if (empty($errors)) {
+	if (empty($errors)) {
 
-				try {
-					$dbclass = new DBClass();
-					$conn = $dbclass->getConnection();
+		try {
+			$dbclass = new DBClass();
+			$conn = $dbclass->getConnection();
 
-					$stmt = $conn->prepare(" INSERT INTO files(owner, name, path, hash) " .
-						" VALUES (:owner, :name, :path, :hash) ");
+			for ($i = 0; $i < $all_files; $i++) {
+			
+				$file_name = $_FILES['files']['name'][$i];
+				// https://stackoverflow.com/questions/37008227/what-is-the-difference-between-name-and-tmp-name
+				$file_tmp = $_FILES['files']['tmp_name'][$i];
+				$file_type = $_FILES['files']['type'][$i];
+				$file_size = $_FILES['files']['size'][$i];
+				// https://stackoverflow.com/questions/173868/how-do-i-get-extract-a-file-extension-in-php
+				$file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+		
+				$file = $folderPath . $file_name;
 
-					$fileHash = hash_file('sha256', $file_tmp);
+				$stmt = $conn->prepare(" INSERT INTO files(owner, name, path, hash) " .
+					" VALUES (:owner, :name, :path, :hash) ");
 
-					$new_file_name = $folderPath . $fileHash . "." . $file_ext;
+				$fileHash = hash_file('sha256', $file_tmp);
+
+				$new_file_name = $folderPath . $fileHash . "." . $file_ext;
+
+				$stmt->bindValue(':owner', $_SESSION['uid'], PDO::PARAM_INT);
+				$stmt->bindValue(':name', $file_name, PDO::PARAM_STR);
+				$stmt->bindValue(':path', $new_file_name, PDO::PARAM_STR);
+				$stmt->bindValue(':hash', $fileHash, PDO::PARAM_STR);
+
+				$stmt->execute();
+
+				if (!move_uploaded_file($file_tmp, $new_file_name)) {
+					$stmt = $conn->prepare(" DELETE FROM files WHERE " .
+					" owner = :owner AND hash = :hash ");
 
 					$stmt->bindValue(':owner', $_SESSION['uid'], PDO::PARAM_INT);
-					$stmt->bindValue(':name', $file_name, PDO::PARAM_STR);
-					$stmt->bindValue(':path', $new_file_name, PDO::PARAM_STR);
 					$stmt->bindValue(':hash', $fileHash, PDO::PARAM_STR);
 
 					$stmt->execute();
 
-					if (!move_uploaded_file($file_tmp, $new_file_name)) {
-						$stmt = $conn->prepare(" DELETE FROM files WHERE " .
-						" owner = :owner AND hash = :hash ");
-	
-						$stmt->bindValue(':owner', $_SESSION['uid'], PDO::PARAM_INT);
-						$stmt->bindValue(':hash', $fileHash, PDO::PARAM_STR);
-	
-						$stmt->execute();
-	
-						$errors['move_uploaded_file'] = "Sorry, there was an error uploading your file " .
-							"on 'move_uploaded_file' function.";
-					}
-				} catch(PDOException $e) {
-					if ($e->errorInfo[1] == 1062) {
-						$errors['already_exists'] = "You have already uploaded this file.";
-					} else {
-						$errors['exception'] = $msg;
-					}
+					$errors['move_uploaded_file'] = "Sorry, there was an error uploading your file " .
+						"on 'move_uploaded_file' function.";
 				}
-
-				$dbclass->closeConnection();
+			}
+		} catch(PDOException $e) {
+			if ($e->errorInfo[1] == 1062) {
+				$errors['already_exists'] = "You have already uploaded this file.";
+			} else {
+				$errors['exception'] = $msg;
 			}
 		}
-	} else {
-		$errors['_FILES'] = "\$_FILES['files'] not set.";
+
+		$dbclass->closeConnection();
 	}
 }
 
