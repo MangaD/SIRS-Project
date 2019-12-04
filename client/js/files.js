22ddd5cc87e1f801f760fb56b6cad99f781ac79e
David Gonçalves <davidmanuel1993@hotmail.com>
2019-12-04 21:26:05 +0000

    cursor pointer

--- client/js/files.js
+++ client/js/files.js
@@ -23,11 +23,13 @@ function loadFiles() {
 				table_html += '<td>' + files[index].created_at + '</td>';
 				table_html += '<td ' +
 					' style="color: #cfc;font-size: x-large;text-shadow: 0px 4px 0px #000;">' +
-					'<i data-hash="' + files[index].hash + '" class="fas fa-download" onclick="fileDownload(this);"></i></td>';
+					'<i data-hash="' + files[index].hash + '" class="fas fa-download" ' +
+					'style="cursor:pointer;" onclick="fileDownload(this);"></i></td>';
 
 				if (window.username === files[index].username) {
 					table_html += '<td style="color: #f77;font-size: x-large;text-shadow: 0px 4px 0px #000;">' +
-						'<i data-hash="' + files[index].hash + '" class="fas fa-trash-alt"></i></td>';
+						'<i data-hash="' + files[index].hash + '" class="fas fa-trash-alt" ' +
+						'style="cursor:pointer;" ></i></td>';
 				} else {
 					table_html += '<td>&nbsp;</td>';
 				}
@@ -48,6 +50,7 @@ function loadFiles() {
 
 function fileDownload(el){
 	let hash = el.getAttribute('data-hash');
+	console.log(hash);
 
 	postJSONData("filesDownload.php", {
 		hash: hash,
